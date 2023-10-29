49b9045cecc0cfb80a092ae59570465a22f71e45
MarcoChanwoo <cwhong8087@gmail.com>
2023-10-30 01:22:12 +0900

    updated

--- src/TodoStore.ts
+++ src/TodoStore.ts
@@ -20,7 +20,34 @@ export default class TodoStore {
 
   addTodo(title: string){
     const item: TodoItem={
-      id:getId()
+      id:getId(),
+      title,
+      completed: false
+    }
+    this.todos.push(item)
+  }
+
+  toggleTodo(id: number) {
+    const index = this.todos.findIndex(item=>item.id===id);
+    if(index>-1) {
+      this.todos[index].completed = !this.todos[index].completed
     }
   }
+
+  get status() {
+    let completed = 0, remaining = 0;
+    this.todos.forEach(todo => {
+      if(todo.completed) {
+        completed++;
+      } else {
+        remaining++;
+      }
+    })
+    return { completed, remaining }
+  }
+}
+
+let id=0;
+function getId(){
+  return id++;
 }
